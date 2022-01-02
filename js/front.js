2e16267f6ce03d734ab9e73ae03be0c967d83ea6
Lokdei <bertgoens@gmail.com>
2022-01-02 13:00:29 +0100

    new font & images

--- js/front.js
+++ js/front.js
@@ -34,7 +34,7 @@ function contactFormAjax () {
         $this[0].reset() // clear form
 
         $('#contact-message')
-          .html('<div class="alert alert-success" role="alert"><button type="button" class="close" data-dismiss="alert"><span aria-hidden="true">×</span><span class="sr-only">Close</span></button>Thank you for getting in touch. We will get back to you soon!</div>')
+          .html('<div class="alert alert-success" role="alert"><button type="button" class="close" data-dismiss="alert"><span aria-hidden="true">×</span><span class="sr-only">Close</span></button>Bedankt voor je bericht!</div>')
           .fadeIn()
       }
       , 'json')
